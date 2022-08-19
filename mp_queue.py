de8a2294af8d5aab8949c649761d7ca6a67dd09c
Justin Liao <jliao@Macbook-Pro-22.local>
2022-08-19 16:18:04 -0500

    Multiprocessing using queue

---
+++ mp_queue.py
@@ -0,0 +1,38 @@
+import multiprocessing as mp
+import os
+import torch
+
+def consumer(queue):
+    print("Consumer Process ID: ", os.getpid())
+    i = 0
+    while True:
+        product = queue.get()
+
+        if product == 'done': break
+
+        print("Consumer Product ", i, " ID: ", id(product))
+
+
+        print("Product ", i, ": ", product)
+        i += 1
+
+def producer(queue):
+    print("Producer Process ID: ", os.getpid())
+    for i in range(10):
+        product = torch.tensor([i,2,3]) if i % 2 == 0 else torch.tensor([i, 4, 5])
+
+        print("Producer Product ", i, " ID: ", id(product))
+
+        queue.put(product)
+
+    queue.put('done')
+
+
+if __name__ == '__main__':
+
+    queue = mp.Queue()
+    consumer_p = mp.Process(target=consumer, args=(queue,))
+
+    consumer_p.start()
+    producer(queue)
+    consumer_p.join()
