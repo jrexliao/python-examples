fb71c8d402976feac99cbab584b4ac15af07766a
Justin Liao <jrexliao@users.noreply.github.com>
2022-08-24 17:07:49 -0500

    Removed unused code

--- mp_pipe.py
+++ mp_pipe.py
@@ -2,42 +2,6 @@ import multiprocessing as mp
 import time, os, random
 
 
-def f(n):
-    print(os.getpid(), "Start ", time.perf_counter())
-    for i in range(0, 100000000):
-        random.random()
-    print(os.getpid(), "End ", time.perf_counter())
-
-"""
-def g(f, a, l):
-    for i in range(10):
-        avail = 0
-        temp = []
-        while avail == 0:
-            l.acquire()
-            print("Lock acquired (Consumer)", time.perf_counter())
-            try:
-                if f.value == 1:
-                    print("Consuming data (batch: ", i, ")", time.perf_counter())
-                    for i in range(len(a)):
-                        temp.append(a[i])
-                    f.value = 0
-                    avail = 1
-                    print("Data consumed", time.perf_counter())
-                else:
-                    print("Consumer Sleeping", time.perf_counter())
-                    time.sleep(1)
-            finally:
-                l.release()
-                print("Lock Released (Consumer)", time.perf_counter())
-        for j in range(10):
-            print(temp[j])
-        print("Artificial wait start (Consumer)", time.perf_counter())
-        for _ in range(10000000):
-            random.random()
-        print("Artificial wait end (Consumer)", time.perf_counter())
-"""
-
 # Define a function to act as the consumer
 def g(l, v, c):
     # Run a predetermined number of loops of consuming data.
@@ -75,37 +39,6 @@ def g(l, v, c):
 
 
 if __name__ == '__main__':
-    """
-	start = time.perf_counter()
-
-	f(1)
-	#f()
-
-	end = time.perf_counter()
-
-	print('Sequential: ', end-start)
-
-	start2 = time.perf_counter()
-
-	p = mp.Process(target=f, args=(1,))
-	q = mp.Process(target=f, args=(2,))
-	#r = mp.Process(target=f, args=(3,))
-	#s = mp.Process(target=f, args=(4,))
-
-	p.start()
-	q.start()
-	#r.start()
-	#s.start()
-	p.join()
-	q.join()
-	#r.join()
-	#s.join()
-
-	end2 = time.perf_counter()
-
-	print('Multiprocess: ', end2-start2)
-	"""
-
 
     start = time.perf_counter()
 
@@ -151,46 +84,3 @@ if __name__ == '__main__':
     end = time.perf_counter()
 
     print('With MP: ', end - start)
-
-
-    """
-    start = time.perf_counter()
-
-    flag = mp.Value('d', 0)
-    arr = mp.Array('d', range(10))
-    lock = mp.Lock()
-    p = mp.Process(target=g, args=(flag, arr, lock))
-
-    p.start()
-    for i in range(10):
-        available = 0
-        temp = []
-        for _ in range(10):
-            temp.append(random.random())
-        while available == 0:
-            lock.acquire()
-            print("Lock acquired (Producer)", time.perf_counter())
-            try:
-                if flag.value == 0:
-                    print("Producing data (batch: ", i, ")", time.perf_counter())
-                    for j in range(len(arr)):
-                        arr[j] = temp[j]
-                    available = 1
-                    flag.value = 1.0
-                    print("Data produced", time.perf_counter())
-                else:
-                    print("Producer Sleeping", time.perf_counter())
-                    time.sleep(1)
-            finally:
-                lock.release()
-                print("Lock released (Producer)", time.perf_counter())
-        print("Artificial wait start (Producer)", time.perf_counter())
-        for _ in range(0, 10000000):
-            random.random()
-        print("Artificial wait end (Producer)", time.perf_counter())
-    p.join()
-
-    end = time.perf_counter()
-
-    print('with MP', end - start)
-    """
