8155bcf4f272e77a9cf59d35cf4804b5feb83232
Justin Liao <jrexliao@users.noreply.github.com>
2022-09-02 04:41:29 -0500

    Added functionality to capture errors in thread_queue.py

--- thread_queue.py
+++ thread_queue.py
@@ -3,7 +3,7 @@ import os
 import torch
 
 # define a function to receive data from the producer, and perform a task.
-def consumer(queue):
+def consumer(q, error_q):
     # print the Consumer's process ID. In contrast to multi-processing, we expect
     # this process ID to be the same as the producer's process ID.
     print("Consumer Process ID: ", os.getpid())
@@ -13,23 +13,40 @@ def consumer(queue):
     # continually run the task we wish the consumer to perform, until we receive
     # a message from the producer telling us to stop
     while True:
-        product = queue.get()
-        if product == 'done': break
+        # utilize "try-except" here to catch any exceptions and prevent hangs.
+        try:
+            # check the error queue to see if the producer encountered an error.
+            # If the producer did encounter an error, break the loop, which will bring
+            # this function to its end.
+            if error_q.qsize() > 0:
+                err = error_q.get()
+                if err == 'Error': break
 
-        # print the object ID of the product we have received. We expect this ID
-        # to be the same as the ID printed by the producer. If it is, it means
-        # that the data put in queue was not copied, and thus did not take
-        # additional memory space.
-        print("Consumer Product ", i, " ID: ", id(product))
+            product = q.get()
+            if product == 'done': break
 
-        # print the product that we received, to check if it matches the product
-        # the producer sent.
-        print("Product ", i, ": ", product)
-        i += 1
+            # print the object ID of the product we have received. We expect this ID
+            # to be the same as the ID printed by the producer. If it is, it means
+            # that the data put in queue was not copied, and thus did not take
+            # additional memory space.
+            print("Consumer Product ", i, " ID: ", id(product))
+
+            # print the product that we received, to check if it matches the product
+            # the producer sent.
+            print("Product ", i, ": ", product)
+            i += 1
+            if i == 4: print(doesnotexist)
+        # If an error occurs, send a message to the producer to let them know an error
+        # has occurred, and then raise the error.
+        except:
+            print("An exception occurred")
+            error_q.put("Error")
+            raise
+
 
 
 # define a function to generate products to send to the consumer
-def producer(queue, n):
+def producer(q, n, error_q):
     # print the Producer's process ID. In contrast to multi-processing, we expect
     # this process ID to be the same as the consumer's process ID.
     print("Producer Process ID: ", os.getpid())
@@ -37,28 +54,28 @@ def producer(queue, n):
     # loop through a predetermined number of iterations, producing some data each
     # time, and sending it to the consumer via a queue.
     for i in range(n):
-        product = torch.tensor([i,2,3]) if i % 2 == 0 else torch.tensor([i, 4, 5])
+        # utilize "try-except" here to catch any exceptions and prevent hangs.
+        try:
+            # check the error queue to see if the consumer encountered an error.
+            # If the consumer did encounter an error, break the loop, which will bring
+            # this function to its end.
+            if error_q.qsize() > 0:
+                err = error_q.get()
+                if err == 'Error': break
 
-        # print the object ID of the product we have generated. We expect this ID
-        # to be the same as the ID printed by the consumer> If it is, it means that
-        # the data put in the queue was not copied, and thus did not take additional
-        # memory space.
-        print("Producer Product ", i, " ID: ", id(product))
-        queue.put(product)
+            product = torch.tensor([i,2,3]) if i % 2 == 0 else torch.tensor([i, 4, 5])
+
+            # print the object ID of the product we have generated. We expect this ID
+            # to be the same as the ID printed by the consumer> If it is, it means that
+            # the data put in the queue was not copied, and thus did not take additional
+            # memory space.
+            print("Producer Product ", i, " ID: ", id(product))
+            q.put(product)
+        except:
+            print("An exception occurred")
+            error_q.put("Error")
+            raise
 
     # once we have generated all the data we want, send a message to the consumer
     # telling it that the task has been completed.
     queue.put('done')
-
-
-if __name__ == '__main__':
-
-    # initialize a queue, a thread, and set a number of iterations.
-    queue = queue.Queue()
-    consumer_t = threading.Thread(target=consumer, args=(queue,))
-    n = 10
-
-    # start to run consumer and producer.
-    consumer_t.start()
-    producer(queue, n)
-    consumer_t.join()
